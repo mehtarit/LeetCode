49003a033dad08303212af48db2df4430cbe7421
Ritika Mehta <mehtarit@umich.edu>
2020-10-02 04:24:38 +0000

    Sync LeetCode submission - Valid Anagram (csharp)

---
+++ problems/valid_anagram/solution.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        
+        var freqArray = new int[26];
+        
+        foreach(var c in s){
+            int index = c-'a';
+            freqArray[index]++;
+        }
+        
+        foreach(var c in t){
+            int index = c-'a';
+            freqArray[index]--;
+            if(freqArray[index] < 0) return false;
+        }
+        
+        return freqArray.Sum() == 0;
+        
+    }
+}
