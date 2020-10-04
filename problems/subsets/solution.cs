9f61a066355a76a260a6914e7ab59f81719f41c8
Ritika Mehta <mehtarit@umich.edu>
2020-10-04 03:50:30 +0000

    Sync LeetCode submission - Subsets (csharp)

--- problems/subsets/solution.cs
+++ problems/subsets/solution.cs
@@ -1,20 +1,18 @@
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
         
-        List<int> numsList = new List<int>(nums);
-        var result = new List<IList<int>>();
-        generateSubsets(numsList, result, 0 , new List<int>());
-        return result;
+        var subSets = new List<IList<int>>();
+        generateSubSets(nums, subSets, new List<int>(), 0);
+        return subSets;
         
     }
     
-    public void generateSubsets(List<int> numsList, List<IList<int>> subSets,  int index, List<int> currentSubSet)
-    {
-        subSets.Add(new List<int>(currentSubSet));
-        for(int i = index; i < numsList.Count; i++){
-            currentSubSet.Add(numsList[i]);
-            generateSubsets(numsList, subSets, i+1, currentSubSet);
-            currentSubSet.Remove(currentSubSet[currentSubSet.Count - 1]);
+    public void generateSubSets(int[] nums, List<IList<int>> subSets, List<int> current, int index){
+        subSets.Add(new List<int>(current));
+        for(int i=index; i< nums.Length; i++){
+            current.Add(nums[i]);
+            generateSubSets(nums, subSets, current, i+1);
+            current.RemoveAt(current.Count-1);           
         }
     }
 }
