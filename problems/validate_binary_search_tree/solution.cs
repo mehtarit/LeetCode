f9fc68479572d950bc0a3bc9273a6f32c27031b4
Ritika Mehta <mehtarit@umich.edu>
2020-10-10 05:47:48 +0000

    Sync LeetCode submission - Validate Binary Search Tree (csharp)

---
+++ problems/validate_binary_search_tree/solution.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsValidBST(TreeNode root) {
+        
+        return IsValidBST(root, Int64.MinValue, Int64.MaxValue);
+        
+    }
+    
+    public bool IsValidBST(TreeNode node, long min, long max){
+        
+        if(node == null) return true;
+        if(node.val <= min || node.val >= max) return false;
+        return IsValidBST(node.left, min, (long)node.val) && IsValidBST(node.right, (long)node.val, max);
+        
+    }
+}
