8039ebccd8793aca38d5a30b7d72c2dee7b227fd
Ritika Mehta <mehtarit@umich.edu>
2020-11-03 02:06:36 +0000

    Sync LeetCode submission - Isomorphic Strings (csharp)

--- problems/isomorphic_strings/solution.cs
+++ problems/isomorphic_strings/solution.cs
@@ -1,23 +1,26 @@
 public class Solution {
     public bool IsIsomorphic(string s, string t) {
         
-        var index = 0;
+        var s_tMap = new Dictionary<char, char>();
         
-        var mapS2T = new Dictionary<char, char>();
-        var mapT2S = new Dictionary<char, char>();
+        for(int i = 0; i < s.Length; i++){
+            var current = s[i];
+            if(!s_tMap.ContainsKey(current)) {
+                s_tMap.Add(current, t[i]);
+                continue;
+            }
+            if(s_tMap[current] != t[i]) return false;           
+        }
         
-        while(index < s.Length){
-            var sChar = s[index];
-            var tChar = t[index];
-            
-            if(!mapS2T.ContainsKey(sChar)) mapS2T.Add(sChar, tChar);
-            else if(mapS2T[sChar] != tChar) return false;
-            
-            if(!mapT2S.ContainsKey(tChar)) mapT2S.Add(tChar, sChar);      
-            else if(mapT2S[tChar] != sChar) return false;
-                        
-            index++;
-                    
+        var t_sMap = new Dictionary<char, char>();
+        
+        for(int i = 0; i < t.Length; i++){
+            var current = t[i];
+            if(!t_sMap.ContainsKey(current)) {
+                t_sMap.Add(current, s[i]);
+                continue;
+            }
+            if(t_sMap[current] != s[i]) return false; 
         }
         
         return true;
