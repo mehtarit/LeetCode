9566f0d350bd3cc794b32b09a4d89709940e849d
Ritika Mehta <mehtarit@umich.edu>
2020-11-23 01:51:23 +0000

    Sync LeetCode submission - Merge Intervals (csharp)

--- problems/merge_intervals/solution.cs
+++ problems/merge_intervals/solution.cs
@@ -1,58 +1,63 @@
 public class Solution {
     
-    public int[] Merge(int min, int max, int[] interval){
-        int[] values = new int[4];
-        values[0] = min;
-        values[1] = max;
-        values[2]= interval[0];
-        values[3] = interval[1];
+    public bool Overlaps(Tuple<int, int> first, Tuple<int,int> second){
         
-        int [] result = new int[2];
-        //Console.WriteLine("min and max vals are: " + values.Min() + " " + values.Max());
-        result[0] = values.Min();
-        result[1] = values.Max();
-        return result;
+        return (second.Item1 >= first.Item1 && second.Item1 <= first.Item2)
+           || (second.Item2 >= first.Item1 && second.Item2 <= first.Item2);
+   
     }
+    
+    public Tuple<int,int> CombinedInterval(Tuple<int,int> first, Tuple<int,int> second){
         
+        var item1 = Math.Min(Math.Min(first.Item1, first.Item2), Math.Min(second.Item1, second.Item2));
+        var item2 = Math.Max(Math.Max(first.Item1, first.Item2), Math.Max(second.Item1, second.Item2));
+        
+        return Tuple.Create(item1, item2);
+    }
+    
     public int[][] Merge(int[][] intervals) {
         
-        if(intervals.Length < 2) return intervals;
+        var intervalTuples = new List<Tuple<int,int>>();
         
-        Array.Sort(intervals, (arr1, arr2) => arr1[0].CompareTo(arr2[0]));
+        for(int i = 0; i < intervals.Length; i++){
+            var first = intervals[i][0];
+            var second = intervals[i][1];         
+            intervalTuples.Add(Tuple.Create(first, second));          
+        }
         
-        var index = 0;
-        var min = intervals[index][0]; //1
-        var max = intervals[index][1]; //3
+        intervalTuples = intervalTuples.OrderBy(x=>x.Item1).ToList();
+        
+        var index =1;
+        Tuple<int,int> current;
+        Tuple<int,int> previous = intervalTuples[0];
         
         var result = new List<Tuple<int,int>>();
         
-        while(index < intervals.Length){
-            
-            if(max >= intervals[index][0]) {
-                var mergedInterval = Merge(min, max, intervals[index]);
-                min = mergedInterval[0]; //1
-                max = mergedInterval[1]; //6
+        while(index < intervalTuples.Count){
+        
+            current =intervalTuples[index];  
+            if(Overlaps(previous, current)){ 
+                previous = CombinedInterval(current, previous);
+                index++;  
+                continue;
             }
-            else{
-                Console.WriteLine("Adding tuple: " + min + " " + max);
-                result.Add(Tuple.Create(min, max));  
-                min = intervals[index][0];//8
-                max = intervals[index][1];//10
-            }
-            
-            index++;
+            result.Add(previous);
+            previous = current;
+            index++;           
         }
         
-        result.Add(Tuple.Create(min, max));
+        result.Add(previous);
         
-        
-        var answer = new int[result.Count][];
-        for(int i =0; i < result.Count; i++){
-            answer[i] = new int[2];
-            answer[i][0] = result[i].Item1;
-            answer[i][1] = result[i].Item2;
+        var resultArray = new int[result.Count][];
+        for(int i = 0; i < resultArray.Length; i++)
+        {
+            resultArray[i] = new int[2];
+            resultArray[i][0] = result[i].Item1;
+            resultArray[i][1] = result[i].Item2;
         }
         
-        return answer;
+        return resultArray;
+        
+             
     }
 }
